b8acd99eeac5fa1d8cf272523a1466b50aa4daa4
sarat jabari <saratjabari@sarats-MacBook-Pro.local>
2025-10-04 10:51:14 +0300

    refactor(FC-21): Use centralized env configuration across files

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -1,34 +1,14 @@
 import { DataSource } from "typeorm";
-import dotenv from "dotenv";
-
+import { env } from "./envConfig";
 import { User } from "../entities/User";
-
-const envFile =
-  process.env.NODE_ENV === "production"
-    ? ".env.prod"
-    : process.env.NODE_ENV === "test"
-      ? ".env.test"
-      : ".env.dev";
-
-dotenv.config({ path: envFile });
-
-function checkEnvVariables(name: string): string {
-  const value = process.env[name];
-  if (!value || value.trim() === "") {
-    throw new Error(
-      `Environment variable ${name} is required and cannot be empty.`
-    );
-  }
-  return value;
-}
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.POSTGRES_USER || "localhost",
-  port: Number(process.env.DB_PORT) || 5432,
-  username: checkEnvVariables("DB_USERNAME"),
-  password: checkEnvVariables("POSTGRES_PASSWORD"),
-  database: checkEnvVariables("POSTGRES_DB"),
+  host: env.POSTGRES_USER || "localhost",
+  port: Number(env.DB_PORT) || 5432,
+  username: env.DB_USERNAME,
+  password: env.POSTGRES_PASSWORD,
+  database: env.POSTGRES_DB,
   synchronize: false,
   entities: [User],
   migrations: ["src/database/migrations/**/*.ts"],
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
-import dotenv from "dotenv";
-
+import { env } from "./configs/envConfig";
 import app from "./app";
 // ? import { initializeApp } from "./index";
 
-const PORT = process.env.PORT || 3000;
-
 const startServer = async () => {
   // ? await initializeApp();
+  const PORT = env.PORT || 3000;
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
